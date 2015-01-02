5c2bfe745a1d708af9ada0f242bb96e7fa4339a0
Rasmus Zakarias <rwl@cs.au.dk>
2015-01-02 17:15:25 +0100

    started final version of rtz14

---
+++ empiricalZK/rtz14/include/rtz14.h
@@ -0,0 +1,53 @@
+#ifndef RTZ14_H
+#define RTZ14_H
+
+#include <common.h>
+#include <osal.h>
+#include <common.h>
+
+typedef struct _rtz14_ {
+
+	/**
+	 * \briefs return true of this instance is a prover. Eg. there exists a witness.
+	 */
+	bool (*isProver)();
+
+
+	/**
+	 *
+	 * \brief Execute the proof as prover listening for clients if {proverIP} is (void*)0; otherwise
+	 * connect to {proverIP} on port {port} expecting to run as verifier with a prover there.
+	 *
+	 * Notice the {witness} in the constructor cannot be (void*)0 AKA NULL if no {proverIP}
+	 * argument is given.
+	 *
+	 * \return true if the proof was successful and the verifier accepted.
+	 *
+	 */
+	bool (*executeProof)(char * proverIP, uint port);
+
+} * Rtz14;
+
+/**
+ *
+ * Create a fresh RTZ ZeroKnowledge instance
+ *
+ * \param oe     - The operating system abstraction
+ *
+ * \param circuit_file - File containing description of the circuit
+ *
+ * \param witness - the witness encoded as eight bit per byte can be omitted
+ *                  if this instance is expected to run a verifier.
+ *
+ * \return a fresh zero knowledge instance.
+ */
+Rtz14 Rtz14_New(OE oe, char * circuit_file, byte * witness);
+
+/**
+ *
+ * Free the resources used by {instance}.
+ *
+ */
+void Rtz14_Destroy(Rtz14 * instance);
+
+#endif
--- empiricalZK/rtz14/src/rtz14.c
+++ empiricalZK/rtz14/src/rtz14.c
@@ -1,2 +1,31 @@
 #include <rtz14.h>
-static char * libname() { return "rtz14"; } 
+#include <coov3.h>
+
+
+typedef struct _rtz14_impl_ {
+
+} * Rtz14Impl;
+
+COO_DCL(Rtz14, bool, isProver);
+COO_DEF_RET_NOARGS(Rtz14,bool,isProver) {
+ return False;
+}
+
+COO_DCL(Rtz14,bool,executeProof,char * ip, uint port);
+COO_DEF_RET_ARGS(Rtz14, bool, executeProof, char * ip; uint port;,ip,port) {
+ return False;
+}
+
+Rtz14 Rtz14_New(OE oe, char * circuit_file, byte * witness) {
+	Rtz14 res = (Rtz14)oe->getmem(sizeof(*res));
+	COO_ATTACH(Rtz14,res,isProver);
+	COO_ATTACH(Rtz14,res,executeProof);
+	return res;
+}
+
+void Rtz14_Destroy(Rtz14 * instance) {
+ Rtz14 i = 0;
+ if (!instance) return;
+ i = *instance;
+
+}
