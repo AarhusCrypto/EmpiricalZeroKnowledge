37f5f6848d9caf8eac0d7fcbf8368d0f89a3d4ee
Rasmus Winther Zakarias <rwl@cs.au.dk>
2015-08-12 09:27:09 +0200

    Started implementation of the circuit parser utility.

--- empiricalZK/rtz14/src/circuittool.c
+++ empiricalZK/rtz14/src/circuittool.c
@@ -6,11 +6,19 @@
  */
 
 #include <osal.h>
-
+#include <circuitparser.h>
+#include <singlelinkedlist.h>
 
 int main() {
 	OE oe = OperatingEnvironment_LinuxNew();
-
+	Tokenizer tk = FunCallTokenizer_New(oe);
+	List circuit = 0;
+	CircuitParser cp = CircuitParser_New(oe,tk);
+	
+	// TODO(rwl): Complete the circuit tool
+	
+	Tokenizer_Destrpy(&tk);
+	CircuitParser_Destroy(&cp);
 	OperatingEnvironment_LinuxDestroy(&oe);
 	return 0;
 }
